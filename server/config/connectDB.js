0a70e0a34b05c16a94835a1decdaf63fdb9c5dd4
vikasm7440 <vikas.mandal@gammaedge.io>
2025-12-11 17:47:51 +0530

    Update DB URL

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/");
+    await mongoose.connect("mongodb+srv://sakshamjain_db_user:0000@cluster0.unw8kvr.mongodb.net/?appName=Cluster0");
 
-    console.log("✅ MongoDB Connected (Localhost)");
+    console.log("✅ MongoDB Connected (Atlas)");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
